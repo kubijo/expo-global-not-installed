8ea07477907df7130bb4a306b256b6fd7e356769
Josef Kubíček <josef.kubicek@braiins.cz>
2025-02-25 13:52:12 +0200

    skia; format

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -3,8 +3,8 @@ import renderer from 'react-test-renderer';
 
 import { ThemedText } from '../ThemedText';
 
-it(`renders correctly`, () => {
-  const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
+it('renders correctly', () => {
+    const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
 });
